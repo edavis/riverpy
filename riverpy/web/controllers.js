70f95c760aad7788292c2fd3411c84a0a5170ed4
Eric Davis <eric@davising.com>
2014-05-18 11:31:46 -0700

    Add refresh icon

--- riverpy/web/controllers.js
+++ riverpy/web/controllers.js
@@ -7,16 +7,30 @@ riverApp.filter('convertDate', function() {
 });
 
 riverApp.controller('RiverController', ['$scope', '$http', function($scope, $http) {
+    $http.get('manifest.js').success(function(obj) {
+	$scope.loading = true;
+	$scope.rivers = obj;
+	$scope.currentRiver = obj[0];
+	$scope.loading = false;
+    });
+
     $scope.$watch('currentRiver', function() {
+	if ($scope.currentRiver) {
+	    $scope.loading = true;
+	    $http.get($scope.currentRiver.url).success(function(obj) {
+		$scope.riverObj = obj;
+	    });
+	    $scope.loading = false;
+	}
+    });
+
+    $scope.refresh = function() {
+	$scope.loading = true;
 	if ($scope.currentRiver) {
 	    $http.get($scope.currentRiver.url).success(function(obj) {
 		$scope.riverObj = obj;
 	    });
 	}
-    });
-
-    $http.get('manifest.js').success(function(obj) {
-	$scope.rivers = obj;
-	$scope.currentRiver = obj[0];
-    });
+	$scope.loading = false;
+    };
 }]);
